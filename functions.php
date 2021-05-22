59466f2882d6f31dacacd0b0eafd120c8afeae4e
David Palma <davidpalma@users.noreply.github.com>
2021-05-22 17:00:09 -0400

    agregadas funciones de usuarios

--- functions.php
+++ functions.php
@@ -1,9 +1,20 @@
 <?php 
 
+/*
+ ██████╗ ██████╗ ███╗   ██╗███████╗██╗  ██╗██╗ ██████╗ ███╗   ██╗
+██╔════╝██╔═══██╗████╗  ██║██╔════╝╚██╗██╔╝██║██╔═══██╗████╗  ██║
+██║     ██║   ██║██╔██╗ ██║█████╗   ╚███╔╝ ██║██║   ██║██╔██╗ ██║
+██║     ██║   ██║██║╚██╗██║██╔══╝   ██╔██╗ ██║██║   ██║██║╚██╗██║
+╚██████╗╚██████╔╝██║ ╚████║███████╗██╔╝ ██╗██║╚██████╔╝██║ ╚████║
+ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+                                                                 
+*/
+
+
 function Conectar()
 { 
  //$con = mysqli_connect("localhost","my_user","my_password","my_db");
-$con = mysqli_connect("localhost","laborato_schalper","schalper2021","laborato_schalper");
+$con = mysqli_connect("localhost","csi42276_schalper","Alh_84;3","csi42276_schalper");
 if (mysqli_connect_errno())  {  echo "Error al conectar MySQL: " . mysqli_connect_error();  }
 return $con;
 }
@@ -13,233 +24,36 @@ function Desconectar($con)
 }
 
 
-//obtiene los datos de la sesión
-function datos_usuario($user,$con)
+/*██╗   ██╗███████╗██╗   ██╗ █████╗ ██████╗ ██╗ ██████╗ ███████╗
+/*██║   ██║██╔════╝██║   ██║██╔══██╗██╔══██╗██║██╔═══██╗██╔════╝
+/*██║   ██║███████╗██║   ██║███████║██████╔╝██║██║   ██║███████╗
+/*██║   ██║╚════██║██║   ██║██╔══██║██╔══██╗██║██║   ██║╚════██║
+/*╚██████╔╝███████║╚██████╔╝██║  ██║██║  ██║██║╚██████╔╝███████║
+/* ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚══════╝
+/*                                                              */
+
+/***********************************************************************************/
+       
+function crear_usuario( $tipo, $nombre, $user, $pass) 
 {
-   
-   $sql = "SELECT * FROM user WHERE usuario='$user'"; 
-   $result=mysqli_query($con,$sql);
-   $info=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    return $info;
+  $fecha_creacion=date("Y-m-d");
+  $activo="Si";
+  $con=Conectar();
+  $sql="INSERT INTO `usuarios` (`id`, `tipo`, `nombre`, `user`, `pass`, `fecha_creacion`, `activo`) VALUES (NULL, '".$tipo."', '".$nombre."', '".$user."', '".$pass.", ".$fecha_creacion.", ".$activo.");";
+  
+   	if ($result=mysqli_query($con,$sql))
+    {
+       return 1; 
+        
+    } 
+    else { return 0;}   
+    
 }
-
-
-function verificar_login($user,$password,$con)
-	{
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM user WHERE usuario='$user' and clave='$password'";
-	  	$result=mysqli_query($con,$sql);
-        $tipo=mysqli_fetch_array($result,MYSQLI_ASSOC);
-        $clase=$tipo['tipo'];
-        mysqli_free_result($result);
-        return $clase;
-         
-	   }
-
-
-      function fechaactual()
-        {
-           $hora= date ("h:i:s");
-           $fecha= date ("Y-m-d"); 
-           $movhoras=6;
-           $fecha2=time(); 
-           $fecha2 = $fecha2-($movhoras * 60 * 60); 
-           $fecha2 = date("Y-m-d", $fecha2 );
-          return($fecha2);
-        }
-
-
-
-
- 
-function datos_producto($producto,$con)
-{
-   
-   $sql = "SELECT * FROM mensajeros WHERE id='$mensajero'"; 
-   $result=mysqli_query($con,$sql);
-   $info=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    return $info;
-}
- 
-
-//obtiene los datos de cliente o proveedor
-function datos_cliente($rut)
-{
-   $con=Conectar();
-   $sql = "SELECT * FROM clientes_y_proveedores WHERE rut='$rut'"; 
-   $result=mysqli_query($con,$sql);
-   $info=mysqli_fetch_array($result,MYSQLI_ASSOC);
-   return $info;
-}
- 
-
-
-
-function datos_ingreso_factura($n_factura)
-{
-    $con=Conectar();
-   $sql = "SELECT * FROM ingreso_factura WHERE n_factura_procedencia='$n_factura'"; 
-   $result=mysqli_query($con,$sql);
-   $info=mysqli_fetch_array($result,MYSQLI_ASSOC);
-   return $info;
-}
- 
-
-
-function existe_ingreso_factura($n_factura)
-	{
-     $con=Conectar();
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM ingreso_factura WHERE n_factura_procedencia='$n_factura'";
-	  	if ($result=mysqli_query($con,$sql))
-  {   $count=mysqli_num_rows($result);}
-    	if($count == 1)
-		{            
-          return 1;
-        }
-		else { return 0; }
-        mysqli_free_result($result);
-	   }
-
-
-function existe($user)
-	{
-     $con=Conectar();
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM user WHERE usuario='$user'";
-	  	if ($result=mysqli_query($con,$sql))
-  {   $count=mysqli_num_rows($result);}
-    	if($count == 1)
-		{            
-          
-            return 1;
-        }
-		else { return 0; }
-        mysqli_free_result($result);
-	   }
-
-
-
-function existe_proveedor($rut)
-	{
-        $con=Conectar();
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM `clientes_y_proveedores` WHERE rut='$rut'";
-	  	if ($result=mysqli_query($con,$sql))
-        {     $count=mysqli_num_rows($result);}
-    	      if($count >= 1)
-		      {            
-               return 1;
-              }
-		      else
-              { 
-              return 0;
-              }
-        mysqli_free_result($result);
-	   }
-
-
-
-
-
-function existe_id($id)
-	{
-     $con=Conectar();
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM usuer WHERE id='$id'";
-	  	if ($result=mysqli_query($con,$sql))
-  {   $count=mysqli_num_rows($result);}
-    	if($count == 1)
-		{            
-          
-            return 1;
-        }
-		else { return 0; }
-        mysqli_free_result($result);
-	   }
-
-
-
-function existe_producto_nombre($nombre)
-	{
-     $con=Conectar();
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM productos WHERE nombre='$nombre'";
-	  	if ($result=mysqli_query($con,$sql))
-  {   $count=mysqli_num_rows($result);}
-    	if($count == 1)
-		{            
-          return 1;
-        }
-		else { return 0; }
-        mysqli_free_result($result);
-	   }
-
-function existe_producto_codigo($codigo)
-	{
-     $con=Conectar();
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM productos WHERE id='$codigo'";
-	  	if ($result=mysqli_query($con,$sql))
-  {   $count=mysqli_num_rows($result);}
-    	if($count == 1)
-		{            
-          return 1;
-        }
-		else { return 0; }
-        mysqli_free_result($result);
-	   }
-
-
-
-function existe_cliente($rut)
-	{
-        $con=Conectar();
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM clientes_y_proveedores WHERE rut='$rut'";
-	  	if ($result=mysqli_query($con,$sql))
-  {   $count=mysqli_num_rows($result);}
-    	if($count == 1)
-		{            
-          return 1;
-        }
-		else { return 0; }
-        mysqli_free_result($result);
-	   }
-
-
-
-
-
-
-function existe_categoria($nombre)
-	{
-     $con=Conectar();
-        $result=0;
-        $count=0;  
-		$sql = "SELECT * FROM categorias WHERE nombre='$nombre'";
-	  	if ($result=mysqli_query($con,$sql))
-  {   $count=mysqli_num_rows($result);}
-    	if($count == 1)
-		{            
-          return 1;
-        }
-		else { return 0; }
-        mysqli_free_result($result);
-	   }
-
+/***********************************************************************************/
 function borrar_usuario($id)
 {
     $con=Conectar();
-    $sql = "DELETE FROM user WHERE id='$id'";
+    $sql = "DELETE FROM usuario WHERE id='$id'";
    	if (mysqli_query($con,$sql))
     {
       if(existe_id($id)==0){return 0;}
@@ -250,141 +64,100 @@ function borrar_usuario($id)
     
 }
 
+/***********************************************************************************/
+function existe($usuario)
+	{
+     $con=Conectar();
+        $result=0;
+        $count=0;  
+		$sql = "SELECT * FROM usuarios WHERE user='$usuario'";
+	  	if ($result=mysqli_query($con,$sql))
+  {   $count=mysqli_num_rows($result);}
+    	if($count == 1)
+		{            
+          
+            return 1;
+        }
+		else { return 0; }
+        mysqli_free_result($result);
+	   }
 
-function borrar_producto($id)
-{
-    $con=Conectar();
-    $sql = "DELETE FROM productos WHERE id='$id'";
-   	if (mysqli_query($con,$sql))
-    {
-      if(existe_mensajero($id)==0){return 0;}
-        else {return 1;}
-        
-    }
-    else {return 1;}
-    
-}
+/***********************************************************************************/
+       function datos_usuario($user,$con)
+       {
+          
+          $sql = "SELECT * FROM usuarios WHERE user='$user'"; 
+          $result=mysqli_query($con,$sql);
+          $info=mysqli_fetch_array($result,MYSQLI_ASSOC);
+           return $info;
+       }
+       
+  /***********************************************************************************/     
+       function verificar_login($usuario,$password,$con)
+           {
+               $result=0;
+               $count=0;  
+               $sql = "SELECT * FROM usuarios WHERE user='$usuario' and pass='$password'";
+               $result=mysqli_query($con,$sql);
+               $tipo=mysqli_fetch_array($result,MYSQLI_ASSOC);
+               $tipo=$tipo['tipo'];
+               mysqli_free_result($result);
+               return $tipo;
+                
+              }
 
-    
+/***********************************************************************************/
 
+       function fechaactual()
+       {
+          $hora= date ("h:i:s");
+          $fecha= date ("Y-m-d"); 
+          $movhoras=6;
+          $fecha2=time(); 
+          $fecha2 = $fecha2-($movhoras * 60 * 60); 
+          $fecha2 = date("Y-m-d", $fecha2 );
+         return($fecha2);
+       }
 
-function crear_usuario( $usuario, $clave, $tipo, $nombre, $email) 
-{
-   $con=Conectar();
-  $sql=" INSERT INTO `user` (`id`, `usuario`, `clave`, `tipo`, `nombre`, `email`) VALUES (NULL, '$usuario', '$clave', '$tipo', '$nombre', '$email');";
-  
-   	if ($result=mysqli_query($con,$sql))
-    {
-       return 1; 
-        
-    } 
-    else { return 0;}   
-    
-}
+/***********************************************************************************/
 
+/*██████╗  █████╗  ██████╗██╗███████╗███╗   ██╗████████╗███████╗███████╗
+/*██╔══██╗██╔══██╗██╔════╝██║██╔════╝████╗  ██║╚══██╔══╝██╔════╝██╔════╝
+/*██████╔╝███████║██║     ██║█████╗  ██╔██╗ ██║   ██║   █████╗  ███████╗
+/*██╔═══╝ ██╔══██║██║     ██║██╔══╝  ██║╚██╗██║   ██║   ██╔══╝  ╚════██║
+/*██║     ██║  ██║╚██████╗██║███████╗██║ ╚████║   ██║   ███████╗███████║
+/*╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚══════╝
+/*                                                                      */
 
-
-function crear_paciente($rut, $nombres, $apellidop, $apellidom, $fnacimiento, $email, $telefono) 
+function crear_paciente($rut, $nombres, $apellido_paterno, $apellido_materno, $fecha_nacimiento, $email, $telefono, $sexo) 
 {
    $con=Conectar();
    $fecha=date("Y-m-d");
  //calcular edad
-    $cumpleanos = new DateTime($fnacimiento);
+    $cumpleanos = new DateTime($fecha_nacimiento);
     $hoy = new DateTime();
     $annos = $hoy->diff($cumpleanos);
     $edad = $annos->y;
     echo "<script>console.log('Edad: ".$edad."' )</scrip>";    
 //setear numero ficha
-$sqlid="select max(id) as id from pacientes";
+$sqlid="select max(id) as id from paciente";
 $resultado_id=mysqli_query($con,$sqlid);
 $id=mysqli_fetch_arra($resultado_id, MYSQLI_ASSOC);
 $ficha=$id['id']+1;
-echo "<script>console.log('Ficha a crear: ".$ficha."' )</scrip>";
-
-
-   $sql="INSERT INTO `paciente` (`id`, `rut`, `nombres`, `apellidop`, `apellidom`, `fecha_registro`, `edad`, `numero_ficha`, `fnacimiento`, `email`, `telefono`) 
-   VALUES (NULL, '$rut', '$nombre', '$apellidop', '$aperllidom', 'current_timestamp()', '$edad', '$ficha', '$fnacimiento', '$email', '$telefono');";  
-
-
+echo "<script>console.log('Ficha a crear: ".$ficha."')</scrip>";
+   $sql="INSERT INTO `paciente` (`rut`, `nombres`, `apellido_paterno`, `apellido_materno`, `fecha_nacimiento`, `email`, `telefono`, `sexo`, `fecha_creacion`) VALUES ('$rut', '$nombres', '$apellido_paterno', '$apellido_materno', '$fecha_nacimiento', '$email', '$telefono', '$sexo', '$fecha');";  
    	if ($result=mysqli_query($con,$sql))
     { return 1; } 
     else { return 0;}    
 }
 
-
-function crear_categoria($nombre) 
-{
-   $con=Conectar();
-    $fecha=date("Y-m-d");
-  $sql=" INSERT INTO `categorias` (`id`, `nombre`) VALUES (NULL,  '$nombre');";  
-   	if ($result=mysqli_query($con,$sql))
-    { return 1; } 
-    else { return 0;}    
-}
+/***********************************************************************************/
 
 
 
 
 
 
-
-
-function  ingreso_factura( $n_factura, $rut, $condicion_pago, $estado_pago, $neto, $iva, $total, $estado, $fecha, $observaciones)    
-   
-{
-  $con=Conectar();
-  $fecha_entrada=explode("/", $fecha);
-  $fecha_corr=$fecha_entrada[2]."-".$fecha_entrada[1]."-".$fecha_entrada[0];  
-  $fecha_salida=date("Y-m-d");
-  $sql="INSERT INTO `ingreso_factura` (`id`, `n_factura_procedencia`, `rut_proveedor`, `condicion_pago`, `estado_pago`, `valor_neto`, `iva`, `total`, `estado`, `fecha`,  `fecha_salida`, `observaciones`) VALUES (NULL, '$n_factura', '$rut', '$condicion_pago', '$estado_pago', '$neto', '$iva', '$total', '$estado', '$fecha_corr', '$fecha_salida', '$observaciones');";  
-   	if ($result=mysqli_query($con,$sql))
-    { return 1; } 
-    else { return 0;}    
-}
-
-
-function  crear_ot($rut, $mecanico, $nombre, $fecha, $direccion, $giro, $telefono, $email, $tipo_vehiculo, $patente, $problema, $observaciones )    
-{
-  $con=Conectar();
-  $fecha_entrada=explode("/", $fecha);
-  $fecha=$fecha_entrada[2]."-".$fecha_entrada[1]."-".$fecha_entrada[0];  
- 
-  $sql="INSERT INTO `ot` (`ot`, `fecha`, `mecanico`, `rut_cliente`, `nombre`, `direccion`, `giro`, `telefono`, `email`, `tipo_vehiculo`, `patente`, `problema`, `observaciones`, `informe`, `valor_mano_obra`, `estado`, `condicion_pago`, `estado_pago`, `factura`,`abono` ) VALUES (NULL, '".$fecha."', '".$mecanico."', '".$rut."', '".$nombre."', '".$direccion."', '".$giro."', '".$telefono."', '".$email."', '".$tipo_vehiculo."', '".$patente."', '".$problema."', '".$observaciones."', NULL, '0', 'Abierta', NULL, NULL, NULL, '0');";
-  
-    
-    
-   	if ($result=mysqli_query($con,$sql))
-    { return 1; } 
-    else { return 0;}    
-}
-
-
-
-function crear_factura( $rut, $nombre, $fecha, $direccion, $giro, $telefono, $email, $observaciones )
-       
-{
-  $con=Conectar();
-  $fecha_entrada=explode("/", $fecha);
-  $fecha=$fecha_entrada[2]."-".$fecha_entrada[1]."-".$fecha_entrada[0];  
- 
-  $sql="INSERT INTO `facturas` (`id`,`factura`, `fecha`,  `rut_cliente`, `nombre`, `direccion`, `giro`, `telefono`, `email`,  `observaciones`, `valor_mano_obra`,`estado`,`condicion_pago`,`estado_pago`,`abono`,`descuento` ) 
-                        VALUES (NULL, NULL, '".$fecha."', '".$rut."', '$nombre', '$direccion', '$giro', '$telefono', '$email',  '$observaciones' , '0', 'Abierta', NULL, 'Pago Pendiente', '0', '0');";  
-   	if ($result=mysqli_query($con,$sql))
-    { return 1; } 
-    else { return 0;}    
-    
-}
-
-
-function crear_proveedor($nombre, $rut, $giro, $telefono, $direccion, $email) 
-{   $con=Conectar();
-    $sql="INSERT INTO `clientes_y_proveedores` (`id`, `nombre`, `rut`, `giro`, `telefono`, `direccion`, `email`) VALUES (NULL, '$nombre', '$rut', ' $giro', '$telefono', '$direccion', '$email');";  
-   	if ($result=mysqli_query($con,$sql))
-    { return 1; } 
-    else { return 0;}    
-}
-   
-$input_arr = array();
 
 
 /*
@@ -421,32 +194,4 @@ function limpiarCadena($valor)
 	$valor = str_ireplace("&","",$valor);
 	return $valor;
 }
-
-
-function hay_stock($producto, $cantidad)
-{
-  $stock_original=0;
-  $stock_salida=0;  
-  $con=conectar();  
-  $sql="select stock from productos where id='$producto'";
-  $info= mysqli_query($con, $sql);
-  $data=mysqli_fetch_array($info, MYSQLI_ASSOC);     
-  $stock_original=$data['stock'];  
-    
-   $sql2="select cantidad_descontada from salida_stock where id_producto='".$producto."' and estado='Abierta'"; 
-   $info2= mysqli_query($con, $sql2);
-  while( $data2=mysqli_fetch_array($info2, MYSQLI_ASSOC)) 
-  { 
-      
-   $stock_salida= $stock_salida + $data2['cantidad_descontada']; 
-      
-  }
-    $stock_salida=$stock_salida+$cantidad;   
-    $stock_restante=$stock_original-$stock_salida;    
-    return $stock_restante;
-    
-}
-
-
-
 ?>
